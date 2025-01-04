976875c00b6916756c60f378d94d12a1e2490ed9
Bence Nagy <nagybence@mailbox.unideb.hu>
2025-01-04 17:54:51 +0100

    Show moderator of topic in TopicHeader

--- src/components/topic/TopicHeader.tsx
+++ src/components/topic/TopicHeader.tsx
@@ -1,6 +1,8 @@
 import { TopicType } from "@/types/TopicType";
 import EditTopicDialog from "../dialogs/EditTopicDialog";
 import DeleteTopicDialog from "../dialogs/DeleteTopicDialog";
+import { UserType } from "@/types/UserType";
+import { Link } from "@nextui-org/react";
 
 export default function TopicHeader({
   topic,
@@ -14,6 +16,17 @@ export default function TopicHeader({
       <div className="flex flex-col gap-y-2">
         <p className="font-semibold text-3xl">{topic?.name}</p>
         <p>{topic?.description}</p>
+        <p className="mt-2">
+          Moderators of topic:{" "}
+          {topic?.moderators.map((moderator: UserType) => (
+            <Link
+              className="font-bold hover:underline"
+              href={`/user/${moderator.id}`}
+            >
+              @{moderator.username}
+            </Link>
+          ))}
+        </p>
       </div>
       {isCurrentUserModeratorOfTopic && (
         <div className="flex gap-x-4 items-center">
